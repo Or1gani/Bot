c0f32b0621a3539248de0e450f3af402ca660ecf
Or1gani <origani-chi@mail.ru>
2024-09-03 15:59:47 +0300

    основа для запуска

--- pythonProject1/main.py
+++ pythonProject1/main.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from aiogram import Dispatcher, Bot, types
 from aiogram.enums import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage
@@ -7,3 +9,10 @@ from important_data.config import TOKEN
 storage = MemoryStorage()
 bot = Bot(token=TOKEN, parse_mode=ParseMode.HTML)
 dp = Dispatcher(storage=storage)
+
+async def main():
+    await dp.start_polling(bot, skip_updates=True)
+
+#bot start
+if __name__ == "__main__":
+    asyncio.run(main())

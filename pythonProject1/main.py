46fee23d941895280bd20a01eeb3af0d3ca9fbd7
Or1gani <origani-chi@mail.ru>
2024-09-03 16:10:48 +0300

    /start команда

--- pythonProject1/main.py
+++ pythonProject1/main.py
@@ -1,14 +1,18 @@
 import asyncio
 
-from aiogram import Dispatcher, Bot, types
-from aiogram.enums import ParseMode
+from aiogram import Dispatcher, Bot, F
+from aiogram.types import Message
 from aiogram.fsm.storage.memory import MemoryStorage
 
 from important_data.config import TOKEN
 
 storage = MemoryStorage()
-bot = Bot(token=TOKEN, parse_mode=ParseMode.HTML)
+bot = Bot(token=TOKEN)
 dp = Dispatcher(storage=storage)
+
+@dp.message(F.text.lower() == "/start")
+async def start_command(message : Message):
+    await message.answer(F"Привет, {message.from_user.first_name}!")
 
 async def main():
     await dp.start_polling(bot, skip_updates=True)
